169f1609914bbc769b88fdf3b9ae4377ee43640e
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-18 07:11:25 +0530

    update

---
+++ 05_HigherOrderFunction/higherOrderFunctionDemo.js
@@ -0,0 +1,34 @@
+/**
+ * Finding area , circumference and diameter of the circle using functional programming
+ */
+const radius = [3, 1, 2, 4];
+
+const area = function (radius){
+    return Math.PI * radius * radius;
+};
+
+const circumference = function(radius){
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function(radius) {
+    return 2 * radius;
+};
+
+const calculate = function (radius,logic){
+    
+    const output = [];
+    for (let i = 0; i < radius.length; i++){
+        output.push(logic(radius[i]));
+    }
+    return output;
+
+};
+
+console.log("Area is: ", calculate(radius,area));
+
+console.log("Circumference is: ", calculate(radius, circumference));
+
+console.log("Diameter is: ", calculate(radius, diameter));
+
+
