9b27bdca2b81c51981014fca7c93b8a68d12e26c
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-18 08:49:05 +0530

    update

--- 05_HigherOrderFunction/filterFunction.js
+++ 05_HigherOrderFunction/filterFunction.js
@@ -15,7 +15,6 @@ const output = arr.filter(isOdd);
 console.log(output);    //Output »»  [ 5, 1, 3, 9, 13 ]
 
 //»» Now we need to filter out all the even values from the array.
-
 const evenValues = arr.filter((x) => x % 2 === 0);      //🔸 We are using arrow function and passing x as parameter which will iterare on each value inside array
 
 console.log(evenValues);    //Output »» [ 2, 6, 4, 12 ]
---
+++ 05_HigherOrderFunction/reduceFunction.js
@@ -0,0 +1,30 @@
+/**
+ * 🚀 What is reduce function ?
+ *     Reduce function is used where we need to take all elements of the array and come out with the single value. For e.g. -- sum of array, find the largest element in the array.  
+ */
+
+const arr = [2, 4, 6, 8, 10];
+
+//»» Now we'll be finding sum of given array. First with without using reduce function.
+function findSum(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(findSum(arr));      //Output »» 30
+
+//🔸 Now using reduce function to sum all the values inside array
+/* The callback function passed inside reduce function takes two paramater first is "accumulator" and the second one is "current". The "current" represent the current value of array and "accumulator" accumulate the values of the array.
+*/
+const sumOutput = arr.reduce(function(acc,curr) {
+    acc = acc + curr;
+    return acc;
+});
+
+console.log(sumOutput);     //»» 30
+
+
+
