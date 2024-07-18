df502f8a38fe38c3a7899081a90aac600ec5f24d
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-18 08:29:48 +0530

    update

---
+++ 05_HigherOrderFunction/filterFunction.js
@@ -0,0 +1,22 @@
+/**
+ * 🚀 What is filter function ?
+ *     Filter function is used to filter the values inside the array.     
+ */
+
+const arr = [5, 1, 3, 2, 6, 4, 9, 13, 12];
+
+//»» We need to filter out all the odd values from the array so here we are creating "isOdd" function first
+function isOdd(x){
+    return x % 2;
+}
+
+const output = arr.filter(isOdd);
+
+console.log(output);    //Output »»  [ 5, 1, 3, 9, 13 ]
+
+//»» Now we need to filter out all the even values from the array.
+
+const evenValues = arr.filter((x) => x % 2 === 0);      //🔸 We are using arrow function and passing x as parameter which will iterare on each value inside array
+
+console.log(evenValues);    //Output »» [ 2, 6, 4, 12 ]
+
--- 05_HigherOrderFunction/mapFunction.js
+++ 05_HigherOrderFunction/mapFunction.js
@@ -11,14 +11,24 @@
  * 
  */
 
-const arr = [2, 4, 6, 8, 1];
+const arr = [5, 1, 3, 2, 6];
 
 //»» Callback function
 function double(x) {
     return 2 * x;
-}
+};
+
+
 
 //◽ So here map function taking function double() as argument and then map is iterating over each value in array. And map function returns an new array which we are storing in output.   
 const output = arr.map(double);
 
 console.log(output);        //»»» Output: [ 4, 8, 12, 16, 2 ]
+
+//»» Here we are passing argument function binary() a callback function inside map() function and stroing the value inside binaryConversion. 
+const binaryConverion = arr.map(function binary(x){
+    return x.toString(2);
+});
+
+console.log(binaryConverion);       //»» Output: [ '101', '1', '11', '10', '110' ]
+
